8d7e6c259eedab2824cad92f278031bf056af7ca
Ryan Kan <97638566+rkanGH@users.noreply.github.com>
2023-01-17 10:11:25 -0800

    Update Test

--- app/src/test/java/edu/ucsd/cse110/lab2/BetterMainActivityTest.java
+++ app/src/test/java/edu/ucsd/cse110/lab2/BetterMainActivityTest.java
@@ -1,2 +1,24 @@
-package edu.ucsd.cse110.lab2;public class BetterMainActivityTest {
+package edu.ucsd.cse110.lab2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+@RunWith(RobolectricTestRunner.class)
+public class BetterMainActivityTest {
+    @Test
+    public void test_two_plus_two_equals_four() {
+        var scenario = ActivityScenario.launch(MainActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+
+        scenario.onActivity(activity -> {
+
+        });
+
+    }
 }
